ba97f16f57e8369869398b5801886f6f12d45c8e
Burmau Garba <burmaud1@gmail.com>
2024-05-19 13:03:58 -0400

    this is just a refactor

--- src/main.go
+++ src/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	mp := make(map[rune]int32, 4)
 	var nodeSlice []*heap.Node
 	var data []byte
 
@@ -17,17 +16,7 @@ func main() {
 		panic(errors.New("no such file or directory"))
 	}
 
-	for i := 0; i < len(data); i++ {
-		if data[i] == '\n' || data[i] == ' ' || data[i] == '\t' {
-			continue
-		}
-		val, in := mp[rune(data[i])]
-		if in {
-			mp[rune(data[i])] = val + 1
-		} else {
-			mp[rune(data[i])] = 1
-		}
-	}
+	mp := makeRuneFrequencyMap(data)
 
 	for k, v := range mp {
 		ch := string(rune(k))
@@ -41,4 +30,20 @@ func main() {
 	for _, node := range nodeSlice {
 		fmt.Printf("Node: char=%c, freq=%d\n", node.GetCh(), node.GetFreq())
 	}
+}
+
+func makeRuneFrequencyMap(data []byte) map[rune]int32 {
+	mp := make(map[rune]int32)
+	for i := 0; i < len(data); i++ {
+		if data[i] == '\n' || data[i] == ' ' || data[i] == '\t' {
+			continue
+		}
+		val, in := mp[rune(data[i])]
+		if in {
+			mp[rune(data[i])] = val + 1
+		} else {
+			mp[rune(data[i])] = 1
+		}
+	}
+	return mp
 }
